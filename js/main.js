906b05b152ec709f72ccf976767340f92cc303bc
Aman <ak193566@gmail.com>
2023-08-18 17:00:38 +0530

    s

--- js/main.js
+++ js/main.js
@@ -128,7 +128,7 @@ $(document).ready(function() {
         responsiveClass: true,
         responsive: {
             0: {
-                items: 2
+                items: 3
             },
             // 600: {
             //     items: 3
